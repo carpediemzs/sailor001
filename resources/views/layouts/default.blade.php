b5d0651b69eb3b509cf677d65df18117ef84d0ef
sailorzs <sailorzs@126.com>
2019-04-22 15:29:45 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title', 'sailor的微博')</title>
+  <title>@yield('title', 'sailor 的微博')</title>
+  <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 
 <body>
-  @yield('content')
+  <nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="{{route('home')}}">Sailor 的微博</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@
 |
 */
 //主页
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')
+    ->name('home');
 //帮助页
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')
+    ->name('help');
 //关于页
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')
+    ->name('about');
