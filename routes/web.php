f57f6f3fddc0b412d624d753a2bbf42151bcf0de
sailorzs <sailorzs@126.com>
2019-04-22 13:33:27 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//主页
+Route::get('/', 'StaticPagesController@home');
+//帮助页
+Route::get('/help', 'StaticPagesController@help');
+//关于页
+Route::get('/about', 'StaticPagesController@about');
