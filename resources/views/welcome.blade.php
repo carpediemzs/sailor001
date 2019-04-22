73cf4f0a02879360d0e345ec2963037994d25a59
sailorzs <sailorzs@126.com>
2019-04-22 06:51:42 +0000

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sailor001
                 </div>
 
                 <div class="links">
