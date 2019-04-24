816b5d9b6dd8b9fc5d4c7c66b8e6297ee965ff99
sailorzs <sailorzs@126.com>
2019-04-24 18:52:53 +0000

    更改用户资料

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,13 +39,27 @@ class UserController extends Controller
     }
 
     //编辑用户个人资料的页面
-    public function edit(){
-
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
     }
 
     //更新用户
-    public function update(){
+    public function update(User $user, Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
 
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password){
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
 
     //显示所有用户列表的页面
